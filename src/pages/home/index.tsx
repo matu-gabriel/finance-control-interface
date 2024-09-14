5394367cc1bcf1ac5ef4bcede7d2799f31bdcb6f
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-09-13 22:32:30 -0300

    Criando e estilizando component title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,24 @@
 import { Button } from "../../components/button";
-import { Header } from "./styles";
+import { Title } from "../../components/title";
+import { Filters, Header, Main, Section } from "./styles";
 
 export function Home() {
   return (
-    <Header>
-      <h1>Finance control</h1>
-      <div>
-        <Button>Nova transação</Button>
-        <Button>Nova categoria</Button>
-      </div>
-    </Header>
+    <>
+      <Header>
+        <h1>Finance control</h1>
+        <div>
+          <Button>Nova transação</Button>
+          <Button>Nova categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receita e despesas dos período" />
+          </Filters>
+        </Section>
+      </Main>
+    </>
   );
 }
