edf26e92e6f4c5df030684815e69ef4a1f936820
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-09-28 15:28:49 -0300

    Criando hook context para autenticar

---
+++ src/hooks/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+
+  const login = () => setisAuthenticated(true);
+  const logout = () => setisAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth deve ser usado dentro de um AuthProvider");
+  }
+  return context;
+};
