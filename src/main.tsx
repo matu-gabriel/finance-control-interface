2af5f970da1c3cb5097a13c32839e4ccbee6bbd9
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-09-28 15:30:47 -0300

    Disponibilizando provider na aplicação toda

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.tsx";
 import GlobalStyle from "./styles/globalStyle.ts";
+import { AuthProvider } from "./hooks/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
