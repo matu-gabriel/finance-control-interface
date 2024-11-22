26f008548cdf1ed6efca5bb53a58e33f1c2ad5f2
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-11-22 16:06:28 -0300

    feat: incluindo modal na transação

--- src/components/transaction/index.tsx
+++ src/components/transaction/index.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { Container, Content, Info } from "./style";
+import { Dialog } from "@mui/material";
+import { EditDialog } from "../edit-dialog";
 
 type TransactionProps = {
-  id: number;
+  id: string;
+  orderNumber: number;
   title: string;
   date: string;
   amount: number;
   category: {
+    _id: string;
     title: string;
     color: string;
   };
@@ -15,26 +20,47 @@ type TransactionProps = {
 
 export function Transaction({
   id,
+  orderNumber,
   title,
   date,
   amount,
   category,
   variant = "receita",
 }: TransactionProps) {
-  return (
-    <Container>
-      <Info>
-        <span>#{id.toString().padStart(4, "0")}</span>
-        <div>
-          <strong>{title}</strong>
-          <span>{date}</span>
-        </div>
-      </Info>
+  const [open, setOpen] = useState(false);
 
-      <Content $variant={variant} $tagColor={category.color}>
-        <strong>{formatCurrency(amount)}</strong>
-        <span>{category.title.toUpperCase()}</span>
-      </Content>
-    </Container>
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Container onClick={handleOpen}>
+        <Info>
+          <span>#{orderNumber.toString().padStart(4, "0")}</span>
+          <div>
+            <strong>{title}</strong>
+            <span>{date}</span>
+          </div>
+        </Info>
+
+        <Content $variant={variant} $tagColor={category.color}>
+          <strong>{formatCurrency(amount)}</strong>
+          <span>{category.title?.toUpperCase()}</span>
+        </Content>
+      </Container>
+      <Dialog open={open} onClose={handleClose}>
+        <EditDialog
+          handleClose={handleClose}
+          transactionId={id}
+          title={title}
+          amount={amount}
+          category={{
+            _id: category._id,
+            title: category.title,
+            color: category.color,
+          }}
+        />
+      </Dialog>
+    </>
   );
 }
