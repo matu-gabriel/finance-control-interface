39ac10d5ed08e9594e1d4e05abe8550b01c07481
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-12-20 18:13:50 -0300

    feat: adicionar rota para a página de gerenciamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/home";
 import { Register } from "../pages/register";
 import { PrivateRoute } from "./PivateRoute";
 import { ErrorPage } from "./ErrorPage";
+import { Manage } from "../pages/manage";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/manage",
+    element: <Manage />,
   },
   {
     path: "/",
